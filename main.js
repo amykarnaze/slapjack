dd3c28c8abf05ecaec041c1f8e58382ba17a0c93
Amy Karnaze <akarnaze@gmail.com>
2020-06-06 22:08:49 -0600

    Add checkSlap function, initial structure of checkWin funtion

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 // var Game = require('./game.js');
 // examine value of suit and grab background img
 // only care about image of top card
+var newGame = new Game();
+newGame.shuffle(newGame.deck);
+newGame.deal();
