fec59209ee824139184031e0faa3251e5fcd9139
Amy Karnaze <akarnaze@gmail.com>
2020-06-08 09:42:21 -0600

    remove leading zeroes

--- card.js
+++ card.js
@@ -3,22 +3,22 @@ class Card {
     this.value = value;
     this.suit = suit;
     this.id = {
-      01: '01',
-      02: '02',
-      03: '03',
-      04: '04',
-      05: '05',
-      06: '06',
-      07: '07',
-      08: '08',
-      09: '09',
+      1: '01',
+      2: '02',
+      3: '03',
+      4: '04',
+      5: '05',
+      6: '06',
+      7: '07',
+      8: '08',
+      9: '09',
       10: '10',
       11: 'jack',
       13: 'queen',
       12: 'king'
     }
     this.src = `${suit}-${this.id[value]}`
-    }
+      }
   }
 //   each card instance can have id w/ numbers and value can be assest
 //
